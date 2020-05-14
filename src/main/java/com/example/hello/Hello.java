782537485b502faf8bcc84b2757d46f38b3dc7e0
JohnyPK <johnypremkumar30@gmail.com>
2020-05-14 20:52:43 +0530

    Update Hello.java

--- src/main/java/com/example/hello/Hello.java
+++ src/main/java/com/example/hello/Hello.java
@@ -3,7 +3,7 @@ package com.example.hello;
 public class Hello {
 	
 	public static void main(String [] args) {
-		System.out.println("Hello, world! Test2");
+		System.out.println("Hello, world! Test3");
 	}
 	
 }
